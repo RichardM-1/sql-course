cbbea424159e94a170e46fc6134c7089392302be
RichardM-1 <rmellings@nhs.net>
2026-09-11 12:59:48 +0000

    Basic select were statement

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,14 @@
 /*
 Example lesson stub
 */
+SELECT ps.PatientId,
+       ps.Hospital,
+       ps.Ward,
+       ps.AdmittedDate,
+       ps.DischargeDate
+FROM   PatientStay AS ps
+WHERE  ps.Hospital IN ('Kingston', 'PRUH')
+       AND ps.Ward LIKE '%S%';
 
-SELECT
-	*
-FROM
-	PatientStay ps ;
+
+-- SELECT DISTINCT ps.hospital FROM patientstay ps
